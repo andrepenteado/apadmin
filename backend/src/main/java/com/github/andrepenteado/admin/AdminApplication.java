ab2c39df891bff9a9323bb43500c39a32f2f2803
André Penteado <andre.penteado@unesp.br>
2023-11-21 23:19:07 -0300

    Criado tabelas no banco do backend

--- backend/src/main/java/com/github/andrepenteado/admin/AdminApplication.java
+++ backend/src/main/java/com/github/andrepenteado/admin/AdminApplication.java
@@ -3,7 +3,7 @@ package com.github.andrepenteado.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.github.andrepenteado.admin", "com.github.andrepenteado.core.web" })
 public class AdminApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/resources/db/changelog/create-user-database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE "apadmin-dbname";
+CREATE USER "apadmin-dbuser" WITH SUPERUSER ENCRYPTED PASSWORD 'apadmin-dbpasswd';
